bb39623ad65a3baa417b5b4de8c158ebf287d356
Masha <iiureva.mariia@gmail.com>
2023-01-18 00:43:39 -0800

    Added NewBoardForm and new board form handler in the app.js

---
+++ src/components/NewBoardForm.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const NewBoardForm = ({ onAddBoardCallback }) => {
+  const [formFields, setFormFields] = useState({
+    title: "",
+    owner: "",
+  });
+
+  const handleTitleChange = (event) => {
+    setFormFields({ ...formFields, title: event.target.value });
+  };
+
+  const handleOwnerChange = (event) => {
+    setFormFields({ ...formFields, owner: event.target.value });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    onAddBoardCallback(formFields.title, formFields.owner);
+
+    setFormFields({
+      title: "",
+      owner: "",
+    });
+  };
+
+  return (
+    <form className="new-board-form__form" onSubmit={handleFormSubmit}>
+      <label htmlFor="boardTitle">Title:</label>
+      <input
+        name="boardTitle"
+        value={formFields.title}
+        onChange={handleTitleChange}
+        required
+      />
+      <label htmlFor="boardOwner">Owner:</label>
+      <input
+        name="boardOwner"
+        value={formFields.owner}
+        onChange={handleOwnerChange}
+        required
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+NewBoardForm.propTypes = {
+  onAddBoardCallback: PropTypes.func.isRequired,
+};
+export default NewBoardForm;
