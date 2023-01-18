6f1eeb679ed02d799843020b62efc306ba3cb46e
Thao Le <thaole0325@gmail.com>
2023-01-18 14:23:37 -0500

    boardlist component finished

---
+++ src/components/BoardList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./BoardList.css";
+
+const BoardList = (props) => {
+  return (
+    <ol className="boards__list">
+      {props.boardsData.map((board) => (
+        <li
+          key={board.boardId}
+          onClick={() => props.onSelectBoard(board.boardId)}
+          title={board.title}
+        >
+          // display board title here, visible on page
+          {board.title}
+        </li>
+      ))}
+      ;
+    </ol>
+  );
+};
+
+Boardlist.propTypes = {
+  boardsData: PropTypes.arrayOf(
+    PropTypes.shape({
+      boardId: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      owner: PropTypes.string.isRequired,
+    })
+  ),
+  onSelectBoard: PropTypes.func.isRequired,
+};
+
+export default BoardList;
