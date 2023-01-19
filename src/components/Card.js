1e8a24aad26365e443690e1e66b877ac841eb5ab
n2020h <uzame.va@gmail.com>
2023-01-19 13:59:38 -0500

    updated Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,61 +1,25 @@
-import React from "react";
-import "./Card.css";
 import PropTypes from "prop-types";
 
 const Card = (props) => {
-  const onLikeButtonClick = () => {
-    props.onLike(props.cardId);
-  };
-
-  const onDeleteButtonClick = () => {
-    props.onDelete(props.cardId);
-  };
-
-  //   const updatedMessage = {
-  //     id: props.id,
-  //     message: props.message,
-  //     liked: !props.liked,
-  //     board_id: props.board_id,
-  //   };
-
-  //   props.onLikeMessage(updatedMessage.id);
-  // };
-
-  // const heartColor = props.liked ? "❤️" : "🤍";
-
   return (
-    <div className="card-entry">
-      <h3 className="entry-message">{props.message}</h3>
-      {/* section to allow for future style choices */}
-      {/* example <section className = "entry-bubble "/> */}
-      <section classname="cards_buttons">
-        <p> {props.likesCount} 💕 </p>
-        <button
-          className="like_button"
-          type="button"
-          onClick={onLikeButtonClick}
-        >
-          +1
-        </button>
-
-        <button
-          className="delete_button"
-          type="button"
-          onClick={onDeleteButtonClick}
-        >
-          Delete
-        </button>
-      </section>
-    </div>
+    <li>
+      <h3>Message: {props.message}</h3>
+      <h3>Likes Count: {props.likesCount}</h3>
+      <button onClick={() => props.onUpdateLikes(props.id)}>Likes Count</button>
+      <button onClick={() => props.onUpdateMessage(props.id)}>
+        Edit Message
+      </button>
+      <button onClick={() => props.onDeleteCard(props.id)}>Delete Card</button>
+    </li>
   );
 };
 
 Card.propTypes = {
-  cardId: PropTypes.number.isRequired,
   message: PropTypes.string.isRequired,
   likesCount: PropTypes.number.isRequired,
-  onLike: PropTypes.func.isRequired,
-  onDelete: PropTypes.func.isRequired,
+  onUpdateLikes: PropTypes.func.isRequired,
+  onUpdateMessage: PropTypes.func.isRequired,
+  onDeleteCard: PropTypes.func.isRequired,
 };
 
 export default Card;
