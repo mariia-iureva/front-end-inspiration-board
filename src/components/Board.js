d614684e7479cee727cefb9fbe4c9605ec82fbf1
Yael <yaelechols@gmail.com>
2023-01-19 09:28:29 -0800

    Unifies naming conventions across Card related components and submits a draft of Board section logic for standup discussion

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import CardList from './CardList';
-import NewCardForm from './NewCardForm';
+// import NewCardForm from './NewCardForm';
 import './Board.css';
 
 const Board = (props) => {
     return (
         <section className='cards__container'>
             <section>
-              <h2>Cards for "title of selected board"</h2>
+              <h2>{props.selectedBoardObj?.title}</h2>
               <CardList 
-                // cardsData={props.cardsData}
+                cardsData={props.cardsData}
               />
             </section>
             {/* <NewCardForm postNewCard={props.postNewCard}/> */}
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,35 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import Card from './Card';
 import './CardList.css';
 
 const CardList = (props) => {
     return (
-        <div className='card-items__container'>
-            <p>This is where we unpack cards passed from App</p>
-            {/* <ul className='card-list'>
-                {props.cardsData.map((card) => (
-                    <li><Card 
-                        card_id={card.card_id}
-                        board_id={card.board_id}
-                        likes_count={card.likes_count}
-                        message={card.message}
-                    /></li>
-                ))}
-            </ul> */}
+        <div className='card-entries__container'>
+            {props.cardsData.map((card) => (
+                <Card 
+                    key={`${card.card_id}-${card.owner}`}
+                    card_id={card.card_id}
+                    board_id={card.board_id}
+                    likes_count={card.likes_count}
+                    message={card.message}
+                />
+            ))}
         </div>
     );
 };
 
-CardList.propTypes = {
-    cardsData: PropTypes.arrayOf(
-        PropTypes.shape({
-            card_id: PropTypes.number.isRequired,
-            board_id: PropTypes.number.isRequired,
-            likes_count: PropTypes.number.isRequired,
-            message: PropTypes.string.isRequired,
-        })
-    )
-};
+// CardList.propTypes = {
+//     cardsData: PropTypes.arrayOf(
+//         PropTypes.shape({
+//             card_id: PropTypes.number.isRequired,
+//             board_id: PropTypes.number.isRequired,
+//             likes_count: PropTypes.number.isRequired,
+//             message: PropTypes.string.isRequired,
+//         })
+//     )
+// };
 
 export default CardList;
